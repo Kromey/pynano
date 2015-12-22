2e508a259d34a29ce20726ecd41f470a8b46461a
Travis Veazey <travisvz@gmail.com>
2015-12-21 16:06:59 -0900

    Added primary properties to User object

--- pynano/user.py
+++ pynano/user.py
@@ -5,3 +5,20 @@ class User(NanoBase):
     # Endpoint URLs for user objects
     _primary_url = 'http://nanowrimo.org/wordcount_api/wc/{name}'
     _history_url = 'http://nanowrimo.org/wordcount_api/wchistory/{name}'
+
+    @property
+    def id(self):
+        return self._fetch_element('uid')
+
+    @property
+    def name(self):
+        return self._fetch_element('uname')
+
+    @property
+    def wordcount(self):
+        return self._fetch_element('user_wordcount')
+
+    @property
+    def winner(self):
+        return self._fetch_element('winner') == 'true'
+
