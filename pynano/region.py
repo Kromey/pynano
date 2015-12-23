784ab1b4c48cbf601970f3de1cdec507a031aac8
Travis Veazey <travisvz@gmail.com>
2015-12-22 16:06:16 -0900

    Region now uses RegionDay for historical data
    Unfortunately it turns out that the region history API does not provide per-day word counts, and we cannot seem to calculate it as the average appears to be based on those who updated their counts that day, but count is total writers in the region!

--- pynano/region.py
+++ pynano/region.py
@@ -1,4 +1,41 @@
 from .base import NanoBase
+from .day import NanoDay
+
+
+class RegionDay(NanoDay):
+    @property
+    def wordcount(self):
+        # The API doesn't provide this, and we can't seem to calculate it
+        # So for now at least just pretend it doesn't exist
+        raise AttributeError("'RegionDay' object has no attribute 'wordcount'")
+
+    @property
+    def min(self):
+        return self._data['min']
+
+    @property
+    def max(self):
+        return self._data['max']
+
+    @property
+    def average(self):
+        return self._data['average']
+
+    @property
+    def stddev(self):
+        return self._data['stddev']
+
+    @property
+    def writers(self):
+        return self._data['count']
+
+    @property
+    def donations(self):
+        return self._data['donations']
+
+    @property
+    def donors(self):
+        return self._data['numdonors']
 
 
 class Region(NanoBase):
@@ -6,6 +43,8 @@ class Region(NanoBase):
     _primary_url = 'http://nanowrimo.org/wordcount_api/wcregion/{name}'
     _history_url = 'http://nanowrimo.org/wordcount_api/wcregionhist/{name}'
 
+    # Use our own Day object
+    _day_class = RegionDay
     # The day field for the history API
     _date_field = 'date'
 
