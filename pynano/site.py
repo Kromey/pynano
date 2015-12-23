1eb879c3dedbed7f985fa53f70f6b9e6c55f4f71
Travis Veazey <travisvz@gmail.com>
2015-12-22 15:50:38 -0900

    Custom SiteDay object for Site
    Provides extended access to the additional data returned by the site history API

--- pynano/site.py
+++ pynano/site.py
@@ -1,10 +1,37 @@
 from .base import NanoBase
+from .day import NanoDay
+
+
+class SiteDay(NanoDay):
+    @property
+    def min(self):
+        return self._data['min']
+
+    @property
+    def max(self):
+        return self._data['max']
+
+    @property
+    def average(self):
+        return self._data['average']
+
+    @property
+    def stddev(self):
+        return self._data['stddev']
+
+    @property
+    def count(self):
+        # TODO: What is this one??
+        return self._data['count']
 
 
 class Site(NanoBase):
     # Endpoint URLs for region objects
     _primary_url = 'http://nanowrimo.org/wordcount_api/wcstatssummary{name}'
     _history_url = 'http://nanowrimo.org/wordcount_api/wcstats{name}'
+
+    # Use our own Day object
+    _day_class = SiteDay
 
     def __init__(self, *args, **kwargs):
         """Site objects don't have a name, but we have to supply one."""
