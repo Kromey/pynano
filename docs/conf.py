a6d09f4e459cd7ad2520218db1381c82f32627bf
Travis Veazey <travisvz@gmail.com>
2015-12-29 10:27:59 -0900

    Custom styles for better quick ref table

--- docs/conf.py
+++ docs/conf.py
@@ -154,6 +154,8 @@ html_theme = 'agogo'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_style = 'extended.css'
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
