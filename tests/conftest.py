553dcbe13744851bb14119af3002196a92a41f99
Travis Veazey <travisvz@gmail.com>
2015-12-30 14:37:35 -0900

    Make pytest fixture use absolute path
    Also applied PEP8 to tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import responses
 
@@ -8,12 +9,16 @@ from pynano import User
 @pytest.fixture(scope='module')
 def kromey():
     """A test User object tied to the user Kromey"""
-    with open('tests/data/user_kromey.xml') as f:
+
+    file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'data/user_kromey.xml')
+    with open(file_path) as f:
         kromey_xml = f.read()
 
     with responses.RequestsMock() as rsps:
         rsps.add(responses.GET, 'http://nanowrimo.org/wordcount_api/wc/kromey',
-                body=kromey_xml, status=200,
-                content_type='application/xml; charset=utf-8')
+                 body=kromey_xml, status=200,
+                 content_type='application/xml; charset=utf-8')
         return User('kromey', prefetch=True)
 
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,13 +1,17 @@
 from pynano import User
 
+
 def test_user_wordcount(kromey):
     assert kromey.wordcount == 64135
+
 
 def test_user_name(kromey):
     assert kromey.name == 'Kromey'
 
+
 def test_user_id(kromey):
     assert kromey.id == '217507'
+
 
 def test_user_winner(kromey):
     assert kromey.winner
