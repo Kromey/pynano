f823378dd2ad67c7da858158fa251e355362ce38
Travis Veazey <travisvz@gmail.com>
2015-12-30 15:38:22 -0900

    Only do PEP8 and PyPi checks on modern Python

--- tests/test_linting.py
+++ tests/test_linting.py
@@ -2,18 +2,24 @@ import sys
 import subprocess
 
 
+# We really only care about code "cleanness" on modern versions
+LINT = sys.version_info >= (3, 4, 0)
+
+
 def test_pep8():
-    result = subprocess.call(['pep8', '--statistics', '--show-source'])
-    assert result == 0
+    if LINT:
+        result = subprocess.call(['pep8', '--statistics', '--show-source'])
+        assert result == 0
 
 
 def test_pypi():
-    result = subprocess.call(['python',
-                              'setup.py',
-                              'check',
-                              '--restructuredtext',
-                              '--strict',
-                              '--metadata',
-                              ])
-    assert result == 0
+    if LINT:
+        result = subprocess.call(['python',
+                                  'setup.py',
+                                  'check',
+                                  '--restructuredtext',
+                                  '--strict',
+                                  '--metadata',
+                                  ])
+        assert result == 0
 
