66eb5093e97ea58b2d98d9f25d3b74920c978f3e
Travis Veazey <travisvz@gmail.com>
2015-12-31 09:57:56 -0900

    Refactor dependencies, conditionally add future
    future is a compatibility layer package that helps run Py3-style code under Py2

--- setup.py
+++ setup.py
@@ -10,12 +10,22 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import sys
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+# Our package dependencies
+deps = [
+    'requests>=2.9.1',
+    'xmltodict>=0.9.2',
+]
+if sys.version_info < (3, 0, 0):
+    # The future package is a compatibility layer between our Py3 code and Py2
+    deps += ['future>=0.15.2']
 
 setup(
     name='pynano',
@@ -69,10 +79,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-            'requests>=2.9.1',
-            'xmltodict>=0.9.2',
-            ],
+    install_requires=deps,
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
