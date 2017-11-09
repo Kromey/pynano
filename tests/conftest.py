3d8cef14848ab8efb16cec5d0edbfc3af182fd5a
Travis Veazey <travisvz@gmail.com>
2017-11-09 12:28:48 -0900

    Add tests for WriteAPI functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
+import hashlib
 import os
 
 
 import pytest
 import responses
+
+
+try:
+    from urllib.parse import parse_qs
+except ImportError:
+    from urlparse import parse_qs
 
 
 @pytest.fixture()
@@ -35,6 +42,40 @@ def kromey_hist_response():
                  'http://nanowrimo.org/wordcount_api/wchistory/kromey',
                  body=kromeyhist_xml, status=200,
                  content_type='application/xml; charset=utf-8')
+
+    return response_wrapper
+
+
+@pytest.fixture()
+def kromey_writeapi():
+    def response_wrapper(rsps):
+        kromey_response()(rsps)
+
+        file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'data/writeapi_kromey.xml')
+        with open(file_path) as f:
+            kromey_data = f.read()
+
+        def request_callback(request):
+            payload = parse_qs(request.body)
+            key = 'abc123'
+
+            h = hashlib.sha1()
+            h.update(key.encode())
+            h.update(payload['name'][0].encode())
+            h.update(payload['wordcount'][0].encode())
+
+            if payload['hash'][0] == h.hexdigest():
+                return (200, {}, kromey_data)
+            else:
+                return (400, {}, 'ERROR hash mismatch')
+
+        rsps.add_callback(
+            responses.PUT,
+            'https://nanowrimo.org/api/wordcount',
+            callback=request_callback,
+            content_type='text/html; charset=utf-8')
 
     return response_wrapper
 
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,3 +1,5 @@
+import pytest
+from requests import HTTPError
 import responses
 
 
@@ -30,4 +32,26 @@ def test_user_winner(kromey_response):
         kromey_response(rsps)
         kromey = User('kromey')
         assert kromey.winner
+
+
+def test_user_wordcount_setter(kromey_writeapi):
+    with responses.RequestsMock() as rsps:
+        kromey_writeapi(rsps)
+        kromey = User('kromey')
+
+        # No key
+        assert kromey.secret_key is None
+        with pytest.raises(ValueError):
+            kromey.wordcount = 12345
+
+        # Wrong key
+        kromey.secret_key = '123abc'
+        assert kromey.secret_key == '******'
+        with pytest.raises(HTTPError):
+            kromey.wordcount = 12345
+
+        # Correct key
+        kromey.secret_key = 'abc123'
+        assert kromey.secret_key == '******'
+        kromey.wordcount = 12345
 
