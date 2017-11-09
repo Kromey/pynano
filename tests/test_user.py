9246078da120d9d2efe7d51005897483ce11c78b
Travis Veazey <travisvz@gmail.com>
2017-11-09 12:56:52 -0900

    Test prefetch, and assert that data not re-fetched

--- tests/test_user.py
+++ tests/test_user.py
@@ -12,6 +12,20 @@ def test_user_wordcount(kromey_response):
         kromey = User('kromey')
         assert kromey.wordcount == 64135
 
+    # We should get no requests going out now that we've fetched
+    with responses.RequestsMock() as rsps:
+        assert kromey.wordcount == 64135
+
+
+def test_user_wordcount_prefetch(kromey_response):
+    with responses.RequestsMock() as rsps:
+        kromey_response(rsps)
+        kromey = User('kromey', prefetch=True)
+
+    # We should get no requests going out now that we've prefetched
+    with responses.RequestsMock() as rsps:
+        assert kromey.wordcount == 64135
+
 
 def test_user_name(kromey_response):
     with responses.RequestsMock() as rsps:
